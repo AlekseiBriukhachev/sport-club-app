eecb31c88a3a32d9a75d68cba51fabb598294627
Aleksei Briukhachev <abriukhachev@trask.cz>
2024-05-31 12:46:43 +0200

    Add list of items and menu

--- app/src/main/java/com/aleksei/clubolympus/AddMember.java
+++ app/src/main/java/com/aleksei/clubolympus/AddMember.java
@@ -1,14 +1,30 @@
 package com.aleksei.clubolympus;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddMember extends AppCompatActivity {
+
+public class AddMember extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private EditText firstNameInput, lastNameInput, groupInput;
+    private Spinner genderSpinner;
+    private int gender = 0;
+    private ArrayAdapter<CharSequence> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,52 @@ public class AddMember extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        firstNameInput = findViewById(R.id.first_name_field);
+        lastNameInput = findViewById(R.id.last_name_field);
+        groupInput = findViewById(R.id.group_field);
+        genderSpinner = findViewById(R.id.gender_spinner);
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selectedGender = parent.getItemAtPosition(position).toString();
+        if (TextUtils.isEmpty(selectedGender)) {
+            gender = switch (selectedGender) {
+                case "Male" -> 1;
+                case "Female" -> 2;
+                default -> 0;
+            };
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        gender = 0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.save_member) {
+            return true;
+        } else if (itemId == R.id.delete_member) {
+            return true;
+        } else if (itemId == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
